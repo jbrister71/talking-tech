6976a274d449ebccb9d46b38d1052d35d556999a
Bell <jacobbrister71@gmail.com>
2022-09-16 20:46:04 -0400

    added post route for comments

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -13,4 +13,17 @@ router.get('/', (req, res) => {
         });
 });
 
+router.post('/', (req, res) => {
+    Comment.create({
+        content: req.body.content,
+        user_id: req.body.user_id,
+        post_id: req.body.post_id
+    })
+        .then(dbCommentData => res.json(dbCommentData))
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+        });
+});
+
 module.exports = router;
