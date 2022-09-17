e5f58d60b688c7aa0b8bd7f67baaf39a226f81fa
Bell <jacobbrister71@gmail.com>
2022-09-16 20:22:58 -0400

    added post and put routes for posts

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -27,4 +27,38 @@ router.get('/', (req, res) => {
         });
 });
 
+router.post('/', (req, res) => {
+    Post.create({
+        title: req.body.title,
+        content: req.body.content,
+        user_id: req.body.user_id
+    })
+        .then(dbPostData => {
+            res.json(dbPostData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+router.put('/:id', (req, res) => {
+    Post.update(req.body, {
+        where: {
+            id: req.params.id
+        }
+    })
+        .then(dbPostData => {
+            if(!dbPostData) {
+                res.status(404).json({ message: 'No post found with this id' });
+                return;
+            }
+            res.json(dbPostData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
 module.exports = router;
