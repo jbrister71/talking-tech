8f4f332ca77f5706fa4df6a884388be7256eee34
Bell <jacobbrister71@gmail.com>
2022-09-16 20:42:12 -0400

    added get route for comments

---
+++ controllers/api/comment-routes.js
@@ -0,0 +1,16 @@
+const router = require('express').Router();
+const { Comment } = require('../../models');
+
+router.get('/', (req, res) => {
+    Comment.findAll({
+        attributes: ['id', 'content', 'user_id', 'post_id'],
+        order: [['created_at', 'DESC']]
+    })
+        .then(dbCommentData => res.json(dbCommentData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+module.exports = router;
